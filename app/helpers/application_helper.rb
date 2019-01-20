9ba47a3ff812a0408e81138380f23963e86ae7f6
Alvin Voo <xoalvinox@gmail.com>
2019-01-21 01:24:36 +0800

    added user show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size:100 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase) # Digest is ruby module
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{options[:size]}"
+    image_tag gravatar_url, alt: user.username, class: "img-circle"
+  end
 end
