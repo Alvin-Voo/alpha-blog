5389403497252384f4531edd4335c18128e27c3b
Alvin Voo <xoalvinox@gmail.com>
2019-01-22 00:37:18 +0800

    added session new and create for user's login

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,7 @@ Rails.application.routes.draw do
   # overwrite the /user/new path to /signup
   get 'signup', to: 'users#new'
   resources :users, except: :new
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
 end
