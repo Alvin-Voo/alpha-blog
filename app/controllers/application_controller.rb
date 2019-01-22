016049805b11e2222695daca93357f95d58321e4
Alvin Voo <xoalvinox@gmail.com>
2019-01-23 01:06:25 +0800

    added helper methods in application_controller, added login, logout, sign up in nav bar. added links restriction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+
+  # methods defined here are available for all child controllers
+
+  helper_method :current_user, :logged_in? # declare these as available helper methods to view as well
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:danger] = "You must be logged in to perform that action"
+      redirect_to root_path
+    end
+  end
+  
 end
